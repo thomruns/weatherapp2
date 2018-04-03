8ff09ef3b68752c1788909c07d0a5dbcb15f0964
Thom David <thomdavid74@gmail.com>
2018-04-03 12:11:11 -0700

    Get weather from API and test

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('app.js');
+// Initialize weather object
+const weather = new Weather('San Diego', 'CA');
+
+// onload event listener to call getWeather function
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// weather.changeLocation('Pittsburgh', 'PA');
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+}
--- weather.js
+++ weather.js
@@ -1,1 +1,21 @@
-console.log('weather.js');
+class Weather {
+  constructor(city, state) {
+    this.apiKey = 'b3366635159cbc48';
+    this.city = city;
+    this.state = state;
+  }
+
+  // fetch weather from API
+  async getWeather() {
+
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+    const responseData = await response.json();
+    return responseData.current_observation;
+  }
+
+  // Change weather location
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
