92eda156d451bc921a2f63a40d2ab3d7eb95e2ce
Thom David <thomdavid74@gmail.com>
2018-04-03 15:07:41 -0700

    Local storage persistence

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Weather App 2
-This weather app uses the Weather Underground API for functionality, Bootstrap and the Cerulean Bootswatch theme for CSS styling.
+This weather app uses the Weather Underground API for functionality, Bootstrap and the Cerulean Bootswatch theme for CSS styling. With the exception of a single line of jQuery, the app is written entirely in vanilla JavaScript ES6.
 * The app controller is in app.js
 * UI functionality is in ui.js
 * The interface to the weather API is weather.js
 * The local storage functionality is in storage.js
 * The UI is in the index.html file.
+
+User can click a UI button to access a modal, in which the desired city and state can be entered. Various pieces of weather data for the selected location is painted to the UI, and that information persists in local storage until a new city/state combination is chosen by the user.
--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+// Initialize storage object
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Initialize weather object
-const weather = new Weather('San Diego', 'CA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
 // Initialize ui object
 const ui = new UI();
 
 // onload event listener to call getWeather function
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Pittsburgh', 'PA');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change location
+  weather.changeLocation(city, state);
+
+  // Set location in local storage
+  storage.setLocationData(city, state);
+
+  // get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
+
+// 
 
 function getWeather() {
   weather.getWeather()
